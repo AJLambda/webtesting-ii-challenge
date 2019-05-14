0a09a090c56f153f46c2f35b2254445176d2d3ea
AJLambda <apj2277@gmail.com>
2019-05-14 16:40:26 -0700

    displays count of strikes

--- baseball-stadium-personnel/src/display/Display.js
+++ baseball-stadium-personnel/src/display/Display.js
@@ -7,10 +7,28 @@ class Display extends Component {
     strikes: 0
   };
 
+  strikeHandler = () => {
+    if (this.state.strikes < 3) {
+      this.setState({
+        ...this.state,
+        strikes: this.state.strikes + 1
+      });
+    } else {
+      this.setState({
+        balls: 0,
+        strikes: 0
+      });
+    }
+  };
+
   render() {
     return (
       <div>
-        <Dashboard />
+        <h1>The Count</h1>
+        <div>
+          <h3>Strikes: {this.state.strikes}</h3>
+        </div>
+        <Dashboard strike={this.strikeHandler} />
       </div>
     );
   }
